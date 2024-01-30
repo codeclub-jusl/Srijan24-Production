f4e5878bfb6cabb764616a8b45f376354b9340eb
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-30 17:18:18 +0530

    ADD: function which will dynamically load scripts

--- src/components/razorpay/BuyProduct.jsx
+++ src/components/razorpay/BuyProduct.jsx
@@ -1,6 +1,33 @@
 'use client'
 import React, { Suspense } from 'react'
 import Loading from '@/components/Login'
+
+/**
+ * Loads script to the document.
+ *
+ * Loading script takes certain amount of time, thus it returns a promise, which is
+ * fulfilled if the script is loaded successfully, else the promise is rejected.
+ *
+ * @param {string | URL} src The URL of the script to be loaded.
+ *
+ * @returns {Promise<boolean>} Whether the script could be loaded successfully or not.
+ */
+function loadScript(src) {
+    return new Promise(resolve => {
+        const script = document.createElement('script')
+        script.src = src
+
+        script.onload = function () {
+            resolve(true)
+        }
+
+        script.onerror = function () {
+            resolve(false)
+        }
+
+        document.appendChild(script)
+    })
+}
 const BuyProduct = () => {
     const [isLoading, setIsLoading] = useState(false)
 
