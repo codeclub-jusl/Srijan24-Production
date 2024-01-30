c6329d5f97033256b5d83a9cc19f3ea9e308b1c8
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-30 17:15:24 +0530

    ADD: api endpoint to initiate order

---
+++ src/app/api/order-details/route.js
@@ -0,0 +1,47 @@
+import Razorpay from 'razorpay'
+import { NextRequest, NextResponse } from 'next/server'
+import { randomUUID, UUID } from 'crypto'
+const razorpay = new Razorpay({
+    key_id: process.env.NEXT_PUBLIC_RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_API_SECRET,
+})
+
+/**
+ *
+ * @param {NextRequest} request
+ */
+export async function POST(request) {
+    const body = await request.json()
+
+    const payment_currency = 'INR'
+    const payment_capture = 1
+
+    // get this detail based on the productDetails found inside body
+    // this amount is in rupee, but options must take amount in paisa
+    const payment_amount = 499
+
+    const options = {
+        amount: payment_amount * 100,
+        currency: payment_currency,
+        payment_capture: payment_capture,
+        receipt: randomUUID(),
+    }
+
+    try {
+        const order = await razorpay.orders.create(options)
+
+        const responseBody = {
+            order_id: order.id,
+            amount: order.amount,
+            description: order.description,
+            currency: order.currency,
+        }
+
+        return NextResponse.json(responseBody, { status: 200 })
+    } catch (error) {
+        return NextResponse.json(
+            { error: 'Could not create order.' },
+            { status: 500 },
+        )
+    }
+}
