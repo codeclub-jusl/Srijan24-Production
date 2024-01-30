db35d3f7c585a0d460c6891a710def05a0beae30
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-30 17:19:27 +0530

    FIX: register and fetch order details

--- src/components/razorpay/BuyProduct.jsx
+++ src/components/razorpay/BuyProduct.jsx
@@ -28,6 +28,51 @@ function loadScript(src) {
         document.appendChild(script)
     })
 }
+
+/** URL from which razorpay client side SDK script will be loaded. */
+const RAZORPAY_CHECKOUT_URL = 'https://checkout.razorpay.com/v1/checkout.js'
+/* fetch the razorpay api key from environment variables. */
+const RAZORPAY_API_KEY = process.env.NEXT_PUBLIC_RAZORPAY_API_KEY
+
+/**
+ * @param {Object} productDetails
+ * Complete information of the merchendise.
+ * @param {"tshirt"} productDetails.type
+ * What kind of product is ordered.
+ * @param {"S" | "M" | "L" | "XL" | "2XL" | "3XL" | undefined} productDetails.size
+ * Size of the product.
+ * @param {"black" | "white"} productDetails.baseColor
+ * Color of the product.
+ */
+async function makePayment(productDetails) {
+    // try to load the razorpay client side SDK into browser.
+    const scriptLoaded = await loadScript(RAZORPAY_CHECKOUT_URL)
+
+    // check whether the razorpay checkout script could be loaded or not.
+    if (!scriptLoaded) {
+        // if razorpay sdk script could not be loaded, show error to the client
+        // and do not proceed further. custom error UI could be implemented.
+        alert('Razorpay SDK failed to load. Are you online?')
+        return
+    }
+
+    try {
+        const dataExtractionURL = new URL('/api/order-details')
+        // Register order and bring payment details from the backend server.
+        const res = await fetch(dataExtractionURL, {
+            method: 'POST',
+            body: JSON.stringify(productDetails),
+        })
+
+        const { order } = await res.json()
+
+        // console.log(order.id)
+
+    } catch (error) {
+        alert('Order could not created. Please try again.' + error)
+        return
+    }
+}
 const BuyProduct = () => {
     const [isLoading, setIsLoading] = useState(false)
 
