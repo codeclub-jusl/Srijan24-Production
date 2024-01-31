b468bacd6e29fdad995f735e537b85aa3dfd8faf
PIYUSH-PC\Piyush Gupta <pkgupta.pe.ug@jadavpuruniversity.in>
2024-01-31 20:22:35 +0530

    Hero:Added a fcontainer

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='container'>
+        <div className="w-[307px] h-[378px] bg-zinc-300 rounded-tl-[175px] rounded-tr-[175px]">
+        <img className="w-[268.10px] h-[116px]" src="https://via.placeholder.com/268x116" />
+            </div>
+            <div className="w-[307px] h-[286.05px] bg-zinc-300 rounded-full" >
+            <div className="w-[280px] h-[280px] bg-gradient-to-b from-indigo-700 to-violet-900 rounded-full shadow-inner" >
+                //Image
+                //124vh is the height of the page
+
+                </div>
+                </div>
+    </div>
+  )
+}
+
+export default Hero
