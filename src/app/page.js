f7fa576ab3cf56ca71badcb8bcd8691e5cc17ebf
Kushaj Mallick <mystinkybutt01@gmail.com>
2024-02-02 22:14:08 +0530

    timeline first draft

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import Navbar from "@/components/Navbar";
 import Cards from "@/components/Cards";
+import Timeline from "@/components/Timeline";
+
 export default function Home() {
+
   return (
     <>
-      <Cards />
+       <Timeline />
     </>
   );
 }
