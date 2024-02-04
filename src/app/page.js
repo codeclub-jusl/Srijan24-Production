c5166cc619c80b3f9fca28a0bdf166af628a4400
praetorian2710 <joti.ban.2710@gmail.com>
2024-02-04 13:47:04 +0530

    fixed logos

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Footer from '@/components/footer/Footer'
 import Hero from '@/components/hero/Hero'
-import Navbar from "@/components/Navbar";
-import Cards from "@/components/Cards";
-import Countdown from '@/components/hero/Countdown/Countdown';
+import Navbar from '@/components/Navbar'
+import Cards from '@/components/Cards'
+import Countdown from '@/components/hero/Countdown/Countdown'
 export default function Home() {
     return (
         <>
             <Hero />
-            <Countdown/>
+            <Countdown />
             {/* <Cards /> */}
             {/* <Footer /> */}
         </>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,30 +2,54 @@ import React from 'react'
 import './style.css'
 import Image from 'next/image'
 import TextAnimation from './Animation/TextAnimation'
+import Countdown from './Countdown/Countdown'
 const Hero = () => {
     return (
         <>
-        <div className="pb-6 sm:pb-8 lg:pb-12">
-  <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-    <section className="flex flex-col items-center">
-      <div className="flex max-w-xl flex-col items-center pb-16 pt-8 lg:pb-48 lg:pt-32">
-        <div className="image-container text-center mb-4 md:mb-6">
-          <img src='/assets/Subtract.png' alt=''className='base-image'/>
-          <img src='/assets/Srijan logo.png' alt=''className='overlay-image' />
-          <div className='circle'>
-                    <img src='/assets/Vector.png' alt=''  width={193.93}  height={194.09} />
-                </div>
-        </div>
+            <div className='pb-6 sm:pb-8 lg:pb-12'>
+                <div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
+                    <section className='flex flex-col items-center'>
+                        <div className='flex max-w-xl flex-col items-center pb-16 pt-8 lg:pb-2 lg:pt-32'>
+                            <div className='image-container text-center mb-1 md:mb-1'>
+                                <img
+                                    src='/assets/Subtract.png'
+                                    alt=''
+                                    className='base-image'
+                                />
+                                <img
+                                    src='/assets/Srijan logo.png'
+                                    alt=''
+                                    className='overlay-image'
+                                />
+                                <div className='circle'>
+                                    <img
+                                        className='lo'
+                                        src='/assets/Vector.png'
+                                        alt=''
+                                        width={193.93}
+                                        height={194.09}
+                                    />
+                                </div>
+                            </div>
 
-        <h1 className="mb-8 text-4xl font-bold text-black sm:text-5xl md:mb-12 md:text-6xl">Explore Our</h1>
-        <TextAnimation/>
-        <h2 className="mb-8 text-4xl font-bold text-black sm:text-5xl md:mb-12 md:text-6xl">events</h2>
-          <a href="#" className="inline-block text-orange-200:rose:600 md:text-base">21st March-24th March</a>
-        </div>
-        <Countdown/>
-    </section>
-  </div>
-</div>
+                            <h1 className='mb-8 text-4xl font-bold text-black sm:text-5xl md:mb-12 md:text-6xl'>
+                                Explore Our
+                            </h1>
+                            <TextAnimation />
+                            <h2 className='mb-8 text-4xl font-bold text-black sm:text-5xl md:mb-12 md:text-6xl'>
+                                events
+                            </h2>
+                            <a
+                                href='#'
+                                className='inline-block text-orange-200:rose:600 md:text-base'
+                            >
+                                21st March-24th March
+                            </a>
+                        </div>
+                        {/* <Countdown /> */}
+                    </section>
+                </div>
+            </div>
         </>
     )
 }
