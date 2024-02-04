922214699b4b7cd03eb2b8300bab443024a14a61
Akash Bag <110753356+akash0708@users.noreply.github.com>
2024-02-04 08:01:48 +0530

    responsive about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,16 +4,20 @@ import about from '../../public/images/about/about.png'
 
 const About = () => {
     return (
-        <section className='about w-full h-screen'>
-            <div className='w-full h-full bg-[#030022] absolute -z-10'>
-                <Image src={about} fill className='relative object-cover' />
+        <section className='about w-full h-fit sm:h-screen'>
+            <div className='w-full h-screen bg-[#030022] fixed sm:absolute -z-10'>
+                <Image
+                    src={about}
+                    fill
+                    className='fixed sm:relative object-cover'
+                />
             </div>
-            <div className='about_content w-full h-full relative -z-0 text-white p-[120px] flex flex-col justify-center gap-12'>
-                <h1 className='relative text-5xl font-normal'>
+            <div className='about_content w-full h-full relative -z-0 text-white p-12 sm:p-[120px] flex flex-col justify-center gap-12'>
+                <h1 className='relative text-4xl sm:text-5xl font-normal'>
                     About{' '}
                     <span className='font-semibold'>Jadavpur University</span>
                 </h1>
-                <p className='relative font-normal text-xl text-justify'>
+                <p className='relative font-normal text-lg sm:text-xl text-justify'>
                     Lorem Ipsum is simply dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy text ever since the 1500s, when an unknown
@@ -25,10 +29,10 @@ const About = () => {
                     passages, and more recently with desktop publishing software
                     like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
-                <h1 className='relative text-5xl font-normal'>
+                <h1 className='relative text-4xl sm:text-5xl font-normal'>
                     About <span className='font-semibold'>Srijan</span>
                 </h1>
-                <p className='relative font-normal text-xl text-justify'>
+                <p className='relative font-normal text-lg sm:text-xl text-justify'>
                     Lorem Ipsum is simply dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy text ever since the 1500s, when an unknown
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+// import './Gallery.css'
 
 const Gallery = () => {
     return (
         <section className='gallery relative bg-red-400 h-screen w-full flex flex-col items-center justify-center p-12'>
-            <div className='relative w-full h-[30rem] bg-white'>
-                
+            <div className='relative grid gap-4 grid-cols-4 grid-rows-2 w-full h-fit rounded-xl overflow-hidden'>
+                <div className='realtive w-full rounded-xl h-64 bg-black col-span-2'>
+                    Hello
+                </div>
+                <div className='realtive w-full rounded-xl h-64 bg-black'>
+                    Hello
+                </div>
+                <div className='realtive w-full rounded-xl h-[33rem] bg-black row-start-1 row-end-3'>
+                    Hello
+                </div>
+                <div className='realtive w-full rounded-xl h-64 bg-black'>
+                    Hello
+                </div>
+                <div className='realtive w-full rounded-xl h-64 bg-black col-span-2'>
+                    Hello
+                </div>
             </div>
         </section>
     )
