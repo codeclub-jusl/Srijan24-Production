6ca06f65695dbd1e7c410b7cab9d630434be06c2
PIYUSH-PC\Piyush Gupta <pkgupta.pe.ug@jadavpuruniversity.in>
2024-02-04 12:19:29 +0530

    Hero:Changes made, further required

--- src/components/hero/Countdown/Countdown.js
+++ src/components/hero/Countdown/Countdown.js
@@ -25,24 +25,24 @@ const Countdown = () => {
     <div className="py-6 sm:py-8 lg:py-12">
   <div className="mx-auto max-w-screen-xl px-4 md:px-8">
     <div className="grid grid-cols-1 gap-4 md:grid-cols-4 lg:gap-8">
-      <div className="flex flex-col items-center justify-center rounded-lg bg-#6100FF p-4 lg:p-8">
+      <div className="flex flex-col items-center justify-center rounded-lg bg-[#6100FF] p-4 lg:p-8">
         <div className="text-xl font-bold text-gray-100 sm:text-2xl md:text-3xl">{days}</div>
-        <div className="text-sm font-semibold sm:text-base">Days</div>
+        <div className="text-sm font-semibold text-gray-100 sm:text-base">Days</div>
       </div>
 
-      <div className="flex flex-col items-center justify-center rounded-lg bg-#6100FF p-4 md:p-8">
+      <div className="flex flex-col items-center justify-center rounded-lg bg-[#6100FF] p-4 md:p-8">
         <div className="text-xl font-bold text-gray-100 sm:text-2xl md:text-3xl">{hours}</div>
-        <div className="text-sm font-semibold sm:text-base">Hours</div>
+        <div className="text-sm font-semibold text-gray-100 sm:text-base">Hours</div>
       </div>
 
-      <div className="flex flex-col items-center justify-center rounded-lg bg-#6100FF p-4 md:p-8">
+      <div className="flex flex-col items-center justify-center rounded-lg bg-[#6100FF] p-4 md:p-8">
         <div className="text-xl font-bold text-gray-100 sm:text-2xl md:text-3xl">{minutes}</div>
-        <div className="text-sm font-semibold sm:text-base">Minutes</div>
+        <div className="text-sm font-semibold text-gray-100 sm:text-base">Minutes</div>
       </div>
 
-      <div className="flex flex-col items-center justify-center rounded-lg bg-#6100FF p-4 md:p-8">
+      <div className="flex flex-col items-center justify-center rounded-lg bg-[#6100FF] p-4 md:p-8">
         <div className="text-xl font-bold text-gray-100 sm:text-2xl md:text-3xl">{seconds}</div>
-        <div className="text-sm font-semibold sm:text-base">Seconds</div>
+        <div className="text-sm font-semibold text-gray-100 sm:text-base">Seconds</div>
       </div>
     </div>
    </div>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,32 +4,29 @@ import Image from 'next/image'
 import TextAnimation from './Animation/TextAnimation'
 const Hero = () => {
     return (
-        <div className='container'>
-            <div className='w-[307px] h-[378px] bg-zinc-100 rounded-tl-[175px] rounded-tr-[175px]'>
-                <img src='/assets/Subtract.png' alt='' />
-                <div className="icon">
-                <img src='/assets/Srijan logo.png' alt='' />
+        <>
+        <div className="pb-6 sm:pb-8 lg:pb-12">
+  <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <section className="flex flex-col items-center">
+      <div className="flex max-w-xl flex-col items-center pb-16 pt-8 lg:pb-48 lg:pt-32">
+        <div className="image-container text-center mb-4 md:mb-6">
+          <img src='/assets/Subtract.png' alt=''className='base-image'/>
+          <img src='/assets/Srijan logo.png' alt=''className='overlay-image' />
+          <div className='circle'>
+                    <img src='/assets/Vector.png' alt=''  width={193.93}  height={194.09} />
                 </div>
-                {/* 124vh is the height of the page */}
-
-                <div className='circle'>
-                    <Image
-                        src='/assets/Vector.png'
-                        alt=''
-                        width={193.93}
-                        height={194.09}
-                    />
-                </div>
-            </div>
-            <div className='events'>
-                <div className='explore'>Explore our</div>
-                <div>
-                    <TextAnimation/>
-                </div>
-                <div className='explore2'>events</div>
-            </div>
-            <div className=''></div>
         </div>
+
+        <h1 className="mb-8 text-4xl font-bold text-black sm:text-5xl md:mb-12 md:text-6xl">Explore Our</h1>
+        <TextAnimation/>
+        <h2 className="mb-8 text-4xl font-bold text-black sm:text-5xl md:mb-12 md:text-6xl">events</h2>
+          <a href="#" className="inline-block text-orange-200:rose:600 md:text-base">21st March-24th March</a>
+        </div>
+        <Countdown/>
+    </section>
+  </div>
+</div>
+        </>
     )
 }
 
