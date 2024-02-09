f95840f6b851a7d974bae79c24e75186420a1cb6
Baidurya <baiduryadebnath@gmail.com>
2024-02-09 17:24:24 +0530

    Created merchendise page

--- src/app/merchendise/Form.jsx
+++ src/app/merchendise/Form.jsx
@@ -1,23 +1,39 @@
-import "./merchendise.css"
+import "./merchendise.css";
+import { FaUserCircle } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaSquarePhone } from "react-icons/fa6";
+import { GrSchedules } from "react-icons/gr";
+import { IoSchool } from "react-icons/io5";
+import { MdHotelClass } from "react-icons/md";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import { IoColorPalette } from "react-icons/io5";
+import { BiSolidTShirt } from "react-icons/bi";
+import { GiMoneyStack } from "react-icons/gi";
 export default function Form(props)
 {
     return <form id="formData" onSubmit={props.handleSubmit}>
         <div className="formComponents">
+            <FaUserCircle size={27} />
             <input type="text" name="name" placeholder="Enter Name"/>
         </div>
         <div className="formComponents">
+            <MdEmail size={27} />
             <input type="email" name="email" placeholder="Enter Email"/>
         </div>
         <div className="formComponents">
+            <FaSquarePhone size={27} />
             <input type="number" name="PhNumber" placeholder="Enter Phone No."/>
         </div>
         <div className="formComponents">
-            <input type="text" name="college" placeholder="Enter College"/>
+            <IoSchool size={27} />
+            <input type="text" name="college" placeholder="Enter College" value="Jadavpur University" disabled/>
         </div>
         <div className="formComponents">
+            <GrSchedules size={27} />
             <input type="text" name="department" placeholder="Enter Department"/>
         </div>
         <div className="formComponents">
+            <MdHotelClass size={27} />
             <select className="formDropdown" name="year" defaultValue={'none'}>
                 <option value="none" disabled hidden>Select Year</option>
                 <option value="1st">1st</option>
@@ -26,19 +42,22 @@ export default function Form(props)
                 <option value="4th">4th</option>
             </select>
         </div>
-        <div className="formComponents">
+        {/* <div className="formComponents">
             <input type="number" name="roll" placeholder="Enter Roll No."/>
-        </div>
+        </div> */}
         <div className="formComponents">
+            <MdDriveFileRenameOutline size={27} />
             <input type="text" name="nameOnTshirt" placeholder="Enter Name to be printed on Tshirt"/>
         </div>
         <div className="formComponents">
+            <IoColorPalette size={27} />
             <select className="formDropdown" name="colour" defaultValue={'none'}>
                 <option value="none" disabled hidden>Select Colour</option>
                 <option value="black">Black</option>
             </select>
         </div>
         <div className="formComponents">
+            <BiSolidTShirt size={27} />
             <select className="formDropdown" name="size" defaultValue={'none'}>
                 <option value="none" disabled hidden>Select Size</option>
                 <option value="S">S(38)</option>
@@ -50,14 +69,15 @@ export default function Form(props)
             </select>
         </div>
         <div className="formComponents">
+            <GiMoneyStack size={27} />
             <select className="formDropdown" name="payment" defaultValue={'none'}>
                 <option value="none" disabled hidden>Select Payment Mode</option>
                 <option value="upi">UPI</option>
             </select>
         </div>
         <div className="formComponents" id="submitForm">
-            <button className="formButtons" style={{backgroundColor : "orangered"}} type="submit">Submit</button>
-            <button className="formButtons" onClick={props.handleClick}>Close</button>
+            <button id="submitOrder" className="formButtons" style={{backgroundColor : "orangered"}} type="submit">Submit</button>
+            <button id="close" className="formButtons" onClick={props.toggleModal}>Close</button>
         </div>
     </form>
 }
--- src/app/merchendise/page.jsx
+++ src/app/merchendise/page.jsx
@@ -3,25 +3,21 @@ import "./merchendise.css"
 import Image from "next/image";
 import { useState , useEffect} from "react";
 import Form from "./Form";
-// import { Modal } from "reactstrap";
 export default function Merchendise()
 {
-    const [imageUrl , setImageUrl] = useState("/images/Picture1.jpg");
-    const [num , setNum] = useState(2);
-    const [flag , handleFlag] = useState(true)
-    const [isInputFocused, setIsInputFocused] = useState(false);
-    const handleInputFocus = () => {
-        setIsInputFocused(true);
-    };
-    
-    const handleInputBlur = () => {
-        setIsInputFocused(false);
-    };
+    const tshirts = ["/images/black_front.png" , "/images/black_back.png" , "/images/tshirt_front.png" , "/images/tshirt_back.png" , "/images/white_front.png" , "/images/white_back.png"]
+    const [imageUrl , setImageUrl] = useState("/images/black_front.png");
+    const [num , setNum] = useState(1);
+    const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
     useEffect(() => {
         const intervalId = setInterval(() => {
-            const updatedCount = num%5 + 1;
+            const updatedCount = (num+1)%6;
             setNum(updatedCount);
-            setImageUrl(`/images/Picture${num}.jpg`)
+            setImageUrl(tshirts[num])
         }, 3000);
         return () => clearInterval(intervalId);
       }, [num]);
@@ -30,18 +26,32 @@ export default function Merchendise()
         event.preventDefault();
         console.log("Submitted");
     }
-    function handleClick()
-    {
-        handleFlag(!flag);
-    }
     return <div className="bg-gradient-to-r from-[#916bcf] to-[#ff3ab0]" id="merchendise-body">
         <header>Introducing you to JUSL Srijan 2024 Merchendise</header>
         <div id="merchendise-main">
-            <div>
-                <Image id="Merchendise-Image" src={imageUrl} height={300} width={300} alt="Picture of the author" />
-                <div id="cost">Only in 399 &#8377;</div>
+            <div id="Image-Container">
+                <Image src={imageUrl} height={300} width={300} alt="Picture of the author" />
+                <div id="picPositions">
+                    <button style={{backgroundColor : num===1 ? "white" : "purple"}} className="tshirtPosition"></button>
+                    <button style={{backgroundColor : num===2 ? "white" : "purple"}} className="tshirtPosition"></button>
+                    <button style={{backgroundColor : num===3 ? "white" : "purple"}} className="tshirtPosition"></button>
+                    <button style={{backgroundColor : num===4 ? "white" : "purple"}} className="tshirtPosition"></button>
+                    <button style={{backgroundColor : num===5 ? "white" : "purple"}} className="tshirtPosition"></button>
+                    <button style={{backgroundColor : num===0 ? "white" : "purple"}} className="tshirtPosition"></button>
+                </div>
             </div>
-            {flag===true ? <button className="formButtons" style={{boxShadow : "0px 0px 4px 4px violet"}} onClick={handleClick}>click to open the form to order</button> : <Form handleClick={handleClick} handleSubmit={handleSubmit}/>}
+            <div id="ButtonContainer">
+                <div id="cost">Only in 399 &#8377;</div>
+                <button id="order" className="formButtons" onClick={toggleModal}>Click to Order</button>
+            </div>
+            {modal && (
+                <div className="modal">
+                    <div onClick={toggleModal} className="overlay"></div>
+                    <div className="modal-content">
+                        <Form handleSubmit={handleSubmit} toggleModal={toggleModal}/>
+                    </div>
+                </div>
+            )}
         </div>
     </div>;
 };
