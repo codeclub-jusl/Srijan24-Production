73614e3392be7b9639118684c9cb37d23efaf803
animesh <animeshtalukdar34@gmail.com>
2024-02-13 12:07:32 +0530

    temp

--- src/app/events/[events_id]/page.jsx
+++ src/app/events/[events_id]/page.jsx
@@ -1,46 +1,73 @@
 import React from 'react'
 
-
-
 export default () => {
-    const  eventData =  {
-        "eventId": "007",
-        "eventName": "Climbon'23",
-        "eventDate": {
-            "prelims": [
-                "April 15, 2023"
-            ],
-            "finals": "April 16, 2023"
+    const eventData = {
+        eventId: '007',
+        eventName: "Climbon'23",
+        eventDate: {
+            prelims: ['April 15, 2023'],
+            finals: 'April 16, 2023',
         },
-        "eventPoster": "/assets/events/CLIMBON.png",
-        "eventType": "misc",
-        "eventHashtags": [
-            "Mountaineering",
-            "Climbing"
+        eventPoster: '/assets/events/CLIMBON.png',
+        eventType: 'misc',
+        eventHashtags: ['Mountaineering', 'Climbing'],
+        eventCoordinators: [
+            'Krishna Mittal  [ 8540062753 ]',
+            'Urwah Jawaid [ 9103158993 ]',
+            'Ashish Kumar Mandal [ 7294925744 ]',
         ],
-        "eventCoordinators": [
-            "Krishna Mittal  [ 8540062753 ]",
-            "Urwah Jawaid [ 9103158993 ]",
-            "Ashish Kumar Mandal [ 7294925744 ]"
+        minMembers: 1,
+        maxMembers: 1,
+        eventDescription: [
+            'CLIMBON is an artificial rock climbing competition organized by Jadavpur University Mountaineering And Hiking Club every year. It is open to all and there is no age or gender bar. No prior experience is required. Any passionate person from any college can participate. The competition would take place under the supervision of esteemed instructors and all the safety gears would also be provided. It is the best place for any adventurer to begin.',
         ],
-        "minMembers": 1,
-        "maxMembers": 1,
-        "eventDescription": [
-            "CLIMBON is an artificial rock climbing competition organized by Jadavpur University Mountaineering And Hiking Club every year. It is open to all and there is no age or gender bar. No prior experience is required. Any passionate person from any college can participate. The competition would take place under the supervision of esteemed instructors and all the safety gears would also be provided. It is the best place for any adventurer to begin."
-        ],
-        "eventRules": [
-            "Climbing routes: The competition may have different climbing routes with varying levels of difficulty, which are marked with colored holds. Climbers must follow the designated route and only use the marked holds. Touching any other part of the wall or using any other holds is not allowed and may result in disqualification.",
-            "Timing: Each climber has a set amount of time to complete the route. The time limit may vary depending on the difficulty level of the route. If the climber does not complete the route within the specified time, their score will be based on the furthest point they reached.",
-            "Scoring: The score is determined by the number of holds the climber reaches and touches during the climb. Climbers are awarded points for each hold they touch. The climber with the highest score in their category wins.",
-            "Categories: Climbers may be grouped into different categories based on their gender. Each category may have different routes and scoring criteria.",
+        eventRules: [
+            'Climbing routes: The competition may have different climbing routes with varying levels of difficulty, which are marked with colored holds. Climbers must follow the designated route and only use the marked holds. Touching any other part of the wall or using any other holds is not allowed and may result in disqualification.',
+            'Timing: Each climber has a set amount of time to complete the route. The time limit may vary depending on the difficulty level of the route. If the climber does not complete the route within the specified time, their score will be based on the furthest point they reached.',
+            'Scoring: The score is determined by the number of holds the climber reaches and touches during the climb. Climbers are awarded points for each hold they touch. The climber with the highest score in their category wins.',
+            'Categories: Climbers may be grouped into different categories based on their gender. Each category may have different routes and scoring criteria.',
             "Safety: Safety is of utmost importance in artificial rock climbing contests. Climbers must follow all safety guidelines and instructions provided by event organizers. Judges may stop a climber's ascent if they feel that the climber is in danger or not following safety rules.",
-            "Sportsmanship: Climbers are expected to display good sportsmanship and respect for fellow competitors. Any unsportsmanlike behavior or actions that endanger others may result in disqualification.",
-            "Equipment: We will provide all the necessary equipment for the competition, but climbers are encouraged to wear well-fitting shoes with a thin sole and good grip, such as PT shoes."
+            'Sportsmanship: Climbers are expected to display good sportsmanship and respect for fellow competitors. Any unsportsmanlike behavior or actions that endanger others may result in disqualification.',
+            'Equipment: We will provide all the necessary equipment for the competition, but climbers are encouraged to wear well-fitting shoes with a thin sole and good grip, such as PT shoes.',
         ],
-        "eventRuleLink": "https://drive.google.com/file/d/1LUraTyqbn8HVO2_sKE_Dh7nvUfi_hJas/view"
-    };
-    
+        eventRuleLink:
+            'https://drive.google.com/file/d/1LUraTyqbn8HVO2_sKE_Dh7nvUfi_hJas/view',
+    }
+
     return (
-      <div>box</div>
+        <div className='l'>
+            <div id='event-name'>{eventData.eventName}</div>
+
+            <img src={eventData.eventPoster} alt={eventData.eventName} />
+            <div id='event-date-prelims'>
+                {' '}
+                {eventData.eventDate.prelims[0]}{' '}
+            </div>
+            <div id='event-date-finals'> {eventData.eventDate.finals} </div>
+            <div id='event-type'> {eventData.eventType} </div>
+            <div id='event-hashtags'>
+                {' '}
+                {eventData.eventHashtags.join(', ')}{' '}
+            </div>
+            <div id='event-coordinators'>
+                {eventData.eventCoordinators.map((coordinator, index) => (
+                    <div key={index}>{coordinator}</div>
+                ))}
+
+                <div id='min-members'> {eventData.minMembers} </div>
+
+                <div id='max-members'> {eventData.maxMembers} </div>
+                <div id='event-description'>
+                    {eventData.eventDescription.map((description, index) => (
+                        <div key={index}>{description}</div>
+                    ))}
+                    <div id='event-rules'>
+                        {' '}
+                        {eventData.eventRules.join(', ')}{' '}
+                    </div>
+                    <div id='event-rule-link'></div>
+                </div>
+            </div>
+        </div>
     )
-  }
+}
