f6cda18f0b7f1787f10fb53d436926f678d2d2e9
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-17 13:49:51 +0530

    ADD: function to update database

--- src/repository/OrderRepository.js
+++ src/repository/OrderRepository.js
@@ -1,5 +1,6 @@
 import {Order} from "@/dao/Order"
 import {GetDB} from "./Database"
+import {ObjectId} from "mongodb"
 
 export class OrderRepository {
     static collectionName = 'orders'
@@ -19,5 +20,21 @@ export class OrderRepository {
         const collection = db.collection(OrderRepository.collectionName)
         const docs = await collection.find({}).toArray()
         return docs
+    }
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {boolean} isVerified 
+     */
+    async updateVerificationStatusByID(id, isVerified) {
+        const db = await GetDB()
+        const collection = db.collection(OrderRepository.collectionName)
+        const result = await collection.updateOne({"_id": new ObjectId(id)}, {
+            $set: {
+                "isVerified": isVerified
+            }
+        })
+        return result
     }
 }
