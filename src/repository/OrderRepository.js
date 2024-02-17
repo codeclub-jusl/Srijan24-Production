7efc11bded414a8b54c5521d10913cafddc64c09
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-17 11:28:34 +0530

    ADD: order repository to access database

---
+++ src/repository/OrderRepository.js
@@ -0,0 +1,23 @@
+import {Order} from "@/dao/Order"
+import {GetDB} from "./Database"
+
+export class OrderRepository {
+    static collectionName = 'orders'
+
+    /**
+     * @param {Order} order 
+     */
+    async insertOrder(order) {
+        const db = await GetDB()
+        const collection = db.collection(OrderRepository.collectionName)
+        const result = await collection.insertOne(order)
+        return result.insertedId
+    }
+
+    async findAll() {
+        const db = await GetDB()
+        const collection = db.collection(OrderRepository.collectionName)
+        const docs = await collection.find({}).toArray()
+        return docs
+    }
+}
