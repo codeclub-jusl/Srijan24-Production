bb370f49ca1bb7329765416ed97cfedebbd3a09c
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-17 11:25:47 +0530

    FIX: optimized transitions

--- src/app/merchandise/page.jsx
+++ src/app/merchandise/page.jsx
@@ -1,70 +1,71 @@
-"use client";
-import "./merchendise.css"
-import Image from "next/image";
-import { useState , useEffect} from "react";
-import Form from "./Form";
-export default function Merchendise()
-{
-    const tshirts = ["/images/black_front.png" , "/images/black_back.png" , "/images/tshirt_front.png" , "/images/tshirt_back.png" , "/images/white_front.png" , "/images/white_back.png"]
-    const [num , setNum] = useState(0);
-    const [modal, setModal] = useState(false);
+'use client'
+import './merchendise.css'
+import Image from 'next/image'
+import { useState, useEffect, useMemo } from 'react'
+import Form from './Form'
+import Link from 'next/link'
+export default function Merchendise() {
+    const tshirts = useMemo(function () {
+        return [
+            '/images/black_front.png',
+            '/images/tshirt_front.png',
+            '/images/tshirt_back.png',
+            '/images/white_front.png',
+            '/images/white_back.png',
+        ]
+    }, [])
 
-function toggleModal()
-{
-    setModal(!modal);
-    if(!modal)
-    {
-        document.body.classList.add('active-modal');
-    }
-    else
-    {
-        document.body.classList.remove('active-modal');
-    }
-};
+    const tshirtsLen = useMemo(
+        function () {
+            return tshirts.length
+        },
+        [tshirts],
+    )
+
+    const [num, setNum] = useState(0)
     useEffect(() => {
         const intervalId = setInterval(() => {
-            const updatedCount = (num+1)%6;
-            setNum(updatedCount);
-        }, 3000);
-        return () => clearInterval(intervalId);
-      }, [num]);
-    function handleSubmit(event)
-    {
-        event.preventDefault();
-        console.log("Submitted");
-    }
-    return <div className="bg-gradient-to-r from-[#25015E] to-[#050027]" id="merchendise-body">
-        <header>JUSL Srijan 2024 <span id="merch">Merchandise</span></header>
-        <div id="merchendise-main">
-            <div id="Image-Button-Container">
-                <div id="ImageHolder">
-                    <Image id="tshirt1" src={tshirts[(num+4)%6]} height={300} width={300} alt="Picture of the author" />
-                    <Image id="tshirt2" src={tshirts[(num+5)%6]} height={300} width={300} alt="Picture of the author" />
-                    <Image id="tshirt3" src={tshirts[num]} height={300} width={300} alt="Picture of the author" />
-                    <Image id="tshirt4" src={tshirts[(num+1)%6]} height={300} width={300} alt="Picture of the author" />
-                    <Image id="tshirt5" src={tshirts[(num+2)%6]} height={300} width={300} alt="Picture of the author" />
+            const updatedCount = (num + 1) % tshirtsLen
+            setNum(updatedCount)
+        }, 3000)
+        return () => clearInterval(intervalId)
+    }, [num])
+
+    return (
+        <div id='merchendise-main'>
+            <div id='Image-Button-Container'>
+                <div id='ImageHolder'>
+                    {tshirts.map((src, idx) => (
+                        <Image
+                            key={src}
+                            id={`tshirt${idx + 1}`}
+                            src={tshirts[(num + idx) % tshirtsLen]}
+                            height={300}
+                            width={300}
+                            alt='Picture of the author'
+                        />
+                    ))}
                 </div>
-                <div id="picPositions">
-                    <button style={{backgroundColor : num===1 ? "white" : "purple"}} className="tshirtPosition"></button>
-                    <button style={{backgroundColor : num===2 ? "white" : "purple"}} className="tshirtPosition"></button>
-                    <button style={{backgroundColor : num===3 ? "white" : "purple"}} className="tshirtPosition"></button>
-                    <button style={{backgroundColor : num===4 ? "white" : "purple"}} className="tshirtPosition"></button>
-                    <button style={{backgroundColor : num===5 ? "white" : "purple"}} className="tshirtPosition"></button>
-                    <button style={{backgroundColor : num===0 ? "white" : "purple"}} className="tshirtPosition"></button>
+                <div id='picPositions'>
+                    {tshirts.map((_, idx) => (
+                        <button
+                            onClick={() => setNum(idx)}
+                            key={idx}
+                            style={{
+                                backgroundColor:
+                                    num === idx ? 'white' : 'purple',
+                            }}
+                            className='tshirtPosition'
+                        />
+                    ))}
                 </div>
             </div>
-            <div id="ButtonContainer">
-                <div id="cost" >Only in 399 &#8377;</div>
-                <button id="order" onClick={toggleModal}>Click to Order</button>
+            <div id='ButtonContainer'>
+                <div className=''>Only at &#8377; 399</div>
+                <Link className='' href='/merchandise/order'>
+                    Click to Order
+                </Link>
             </div>
-            {modal && (
-                <div className="modal">
-                    <div onClick={toggleModal} className="overlay"></div>
-                    <div className="modal-content">
-                        <Form handleSubmit={handleSubmit} toggleModal={toggleModal}/>
-                    </div>
-                </div>
-            )}
         </div>
-    </div>;
-};
+    )
+}
