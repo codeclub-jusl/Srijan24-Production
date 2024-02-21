2567245bd1013b9e3dfd7926ef95b69b5f3dd334
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-22 00:39:24 +0530

    ADD: cookie for firebase auth token

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,8 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
   user: null,
 };
+
+const getCookie = (name) => {
+  return document.cookie.split(';').some(c => {
+    return c.trim().startsWith(name + '=');
+  });
+}
+
+
+const deleteCookie = (name, path, domain) => {
+  if(getCookie(name)) {
+    document.cookie = name + "=" +
+      ((path) ? ";path=" + path : "") +
+      ((domain) ? ";domain=" + domain : "") +
+      ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+  }
+}
 
 export const userSlice = createSlice({
   name: "user",
@@ -10,13 +26,22 @@ export const userSlice = createSlice({
   reducers: {
     loginUser: (state, action) => {
       state.user = action.payload;
-      document.cookie = `x-srijan-firebase-auth-token=${action.payload.authTokenID}; path=/admin`
+      console.log('before cookies set')
+      document.cookie = `x-srijan-firebase-auth-token=${action.payload.authTokenID}`
+      document.cookie = `x-srijan-firebase-user-email=${action.payload.email}`
+      console.log(action.payload)
+      console.log('after cookies set')
     },
 
     logoutUser: (state, action) => {
       state.user = null;
+      console.log('before cookie removal')
+      deleteCookie('x-srijan-firebase-auth-token')
+      deleteCookie('x-srijan-firebase-user-email')
+      console.log('after cookie removal')
+
     },
   },
 });
 
-export const { loginUser, logoutUser } = userSlice.actions;
+export const {loginUser, logoutUser} = userSlice.actions;
