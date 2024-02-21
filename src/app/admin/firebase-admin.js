0151d8d3845527b2a8c792d4ccc4c05ae85fbb1d
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-22 01:15:08 +0530

    EXPORT: firebase admin sdk setup

--- src/app/admin/firebase-admin.js
+++ src/app/admin/firebase-admin.js
@@ -1,11 +1,28 @@
-import {initializeApp} from 'firebase-admin/app';
+import {applicationDefault, initializeApp, getApps, App} from 'firebase-admin/app';
 import {getAuth} from 'firebase-admin/auth'
 import {getDatabase} from 'firebase-admin/database'
 
-// Initialize the default app
-const defaultApp = initializeApp(defaultAppConfig);
 
-console.log(defaultApp.name);  // '[DEFAULT]'
+const apps = getApps()
+
+/**
+ * @type {App}
+ */
+let defaultApp
+
+for(const app of apps) {
+    if(app.name === 'firebase_admin_app') {
+        defaultApp = app
+    }
+}
+
+if(!defaultApp) {
+    // Initialize the default app
+    defaultApp = initializeApp({
+        credential: applicationDefault(),
+        databaseURL: process.env.FIREBASE_REALTIME_DATABASE_URL
+    }, 'firebase_admin_app');
+}
 
 const defaultAuth = getAuth(defaultApp);
 const defaultDatabase = getDatabase(defaultApp);
