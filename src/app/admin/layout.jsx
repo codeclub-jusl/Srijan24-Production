746b72cfaab8e7c27aefd5ec2e0cdbfd6eda56af
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-22 01:23:51 +0530

    ADD: server component to validate whether user is authenticated to access admin page or not

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,3 +1,44 @@
-export default function AdminLayout({ children }) {
-    return children
+import { cookies } from 'next/headers'
+import { defaultAuth } from './firebase-admin'
+export const runtime = 'nodejs'
+export default async function AdminLayout({ children }) {
+    'use server'
+    const cookieStore = cookies()
+
+    const authCookie = cookieStore.get('x-srijan-firebase-auth-token')
+    const emailCookie = cookieStore.get('x-srijan-firebase-user-email')
+
+    if (!authCookie || !emailCookie) {
+        return <h1>Unauthorized, Sign Up to Continue...</h1>
+    }
+
+    const adminEmailsStr = process.env.ADMIN_EMAIL_ADDRESSES
+    const adminEmails =
+        !adminEmailsStr || adminEmailsStr === ''
+            ? []
+            : adminEmailsStr.split(',')
+
+    if (adminEmails.indexOf(emailCookie.value) < 0) {
+        return <h1>Email {emailCookie.value} not of valid admin...</h1>
+    }
+
+    try {
+        const decodedToken = await defaultAuth.verifyIdToken(
+            authCookie.value,
+            true,
+        )
+
+        if (decodedToken.email !== emailCookie.value) {
+            throw new Error('user faking email...')
+        }
+
+        return children
+    } catch (e) {
+        return (
+            <section>
+                <h2>User Not Authenticated.</h2>
+                <p>o_o This Action Will Be Repoted. o_o</p>
+            </section>
+        )
+    }
 }
