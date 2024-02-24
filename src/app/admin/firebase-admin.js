00dae703bb0c2526de84c10c4539226fd0f092e5
Arindam Halder <halderarindam10000@gmail.com>
2024-02-24 16:29:10 +0530

    :ambulance: firebase-admin commented
    firebase-admin commented

--- src/app/admin/firebase-admin.js
+++ src/app/admin/firebase-admin.js
@@ -1,30 +1,30 @@
-import {applicationDefault, initializeApp, getApps, App} from 'firebase-admin/app';
-import {getAuth} from 'firebase-admin/auth'
-import {getDatabase} from 'firebase-admin/database'
+// import {applicationDefault, initializeApp, getApps, App} from 'firebase-admin/app';
+// import {getAuth} from 'firebase-admin/auth'
+// import {getDatabase} from 'firebase-admin/database'
 
 
-const apps = getApps()
+// const apps = getApps()
 
-/**
- * @type {App}
- */
-let defaultApp
+// /**
+//  * @type {App}
+//  */
+// let defaultApp
 
-for(const app of apps) {
-    if(app.name === 'firebase_admin_app') {
-        defaultApp = app
-    }
-}
+// for(const app of apps) {
+//     if(app.name === 'firebase_admin_app') {
+//         defaultApp = app
+//     }
+// }
 
-if(!defaultApp) {
-    // Initialize the default app
-    defaultApp = initializeApp({
-        credential: applicationDefault(),
-        databaseURL: process.env.FIREBASE_REALTIME_DATABASE_URL
-    }, 'firebase_admin_app');
-}
+// if(!defaultApp) {
+//     // Initialize the default app
+//     defaultApp = initializeApp({
+//         credential: applicationDefault(),
+//         databaseURL: process.env.FIREBASE_REALTIME_DATABASE_URL
+//     }, 'firebase_admin_app');
+// }
 
-const defaultAuth = getAuth(defaultApp);
-const defaultDatabase = getDatabase(defaultApp);
+// const defaultAuth = getAuth(defaultApp);
+// const defaultDatabase = getDatabase(defaultApp);
 
-export {defaultAuth, defaultDatabase}
+// export {defaultAuth, defaultDatabase}
