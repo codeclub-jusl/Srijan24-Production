15024dafa254000256c88e0580781678a6652b87
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-25 07:36:45 +0530

    optimized: extracted components

---
+++ src/app/merchandise/components/OrderGuidelines.jsx
@@ -0,0 +1,150 @@
+'use client'
+
+import Image from 'next/image'
+import Modal from 'react-modal'
+import { TfiHandPointRight } from 'react-icons/tfi'
+
+export default function OrderGuidelines(props) {
+    const customEventModalStyles = {
+        overlay: {
+            background: 'rgba(0,0,0,0.65)',
+            zIndex: '100',
+        },
+    }
+    return (
+        <Modal
+            isOpen={props.visibleInstructionsModal}
+            onRequestClose={() => {
+                props.setVisibleInstructionsModal(
+                    visibleInstructionsModal => !visibleInstructionsModal,
+                )
+            }}
+            className={'instructionsModal'}
+            ariaHideApp={false}
+            style={customEventModalStyles}
+            closeTimeoutMS={700}
+        >
+            <div>
+                <h2>Important Guidelines</h2>
+                <div className={'paymentScannerBox'}>
+                    <div>
+                        <Image
+                            src={'/assets/merchandise/qr_code.webp'}
+                            height={300}
+                            width={300}
+                            alt='qr_image'
+                            className={'qrScannerImage'}
+                            draggable={false}
+                            priority={true}
+                        />
+                        <h4>Ayush Mishra</h4>
+                        <h5>ayushdtps@oksbi</h5>
+                    </div>
+                    <div>
+                        <Image
+                            src={'/assets/merchandise/qr_code.webp'}
+                            height={300}
+                            width={300}
+                            alt='qr_image'
+                            className={'qrScannerImage'}
+                            draggable={false}
+                            priority={true}
+                        />
+                        <h4>Bitan Banerjee</h4>
+                        <h5>bbanerjeeagp@oksbi</h5>
+                    </div>
+                </div>
+                {/* <div className={'instructions'}>
+                        <TfiHandPointRight className={'instructionBullets'} />{' '}
+                        <span>
+                            {' '}
+                            You can order your merchandise in both - Offline &
+                            Online mode{' '}
+                        </span>
+                    </div> */}
+                {/* <div className={styles.instructions}>
+              <TfiHandPointRight className={styles.instructionBullets}/> <span> To order in offline mode: please contact with <strong>Trishit Pal</strong> [ 1234567890 ] or <strong>Ayush Mishra</strong> [ 9876543210 ] </span>
+            </div> */}
+                {/* <div className={'instructions'}>
+                        <TfiHandPointRight className={'instructionBullets'} />{' '}
+                        <span>
+                            At first, enter the details such as name, email,
+                            mobile, college, dept in the Merchandise Form
+                        </span>
+                    </div> */}
+                <div className={'instructions'}>
+                    <TfiHandPointRight className={'instructionBullets'} />{' '}
+                    <span>
+                        Enter the name you want to print upon your SRIJAN
+                        Merchandise. If you don't want any name printed upon the
+                        Tshirt, write "NA"
+                    </span>
+                </div>
+                <div className={'instructions'}>
+                    <TfiHandPointRight className={'instructionBullets'} />{' '}
+                    <span>
+                        If you want to order via Cash, select "CASH" and
+                        complete the payment to one of the Payment Collectors
+                        within a day
+                    </span>
+                </div>
+                <div className={'instructions'}>
+                    <TfiHandPointRight className={'instructionBullets'} />{' '}
+                    <span>
+                        If you want to make the payment via UPI, please select
+                        "UPI" and complete the payment to one of the Payment
+                        Collector within a day via <strong>Google Pay</strong>,{' '}
+                        <strong>Phone Pe</strong>, <strong>Amazon Pay</strong>,{' '}
+                        <strong>Paytm</strong> or <strong>Whatsapp Pay</strong>
+                    </span>
+                </div>
+
+                <div className={'instructions'}>
+                    <TfiHandPointRight className={'instructionBullets'} />{' '}
+                    <span>
+                        If you select method: "UPI", 1 more field will be
+                        visible where you have to give the Transaction ID. [{' '}
+                        <strong>PS:</strong> provide the 12 digit unique numeric
+                        number (UTR) e.g. <i>123456789012</i> ]
+                    </span>
+                </div>
+                <div className={'instructions'}>
+                    <TfiHandPointRight className={'instructionBullets'} />{' '}
+                    <span>
+                        Now select Campus where you want to complete your
+                        payment and then place your order
+                    </span>
+                </div>
+                <div className={'instructions'}>
+                    <TfiHandPointRight className={'instructionBullets'} />{' '}
+                    <span>
+                        <b>Congrats! Your order has been placed.</b> You can
+                        check the order status in your Dashboard
+                    </span>
+                </div>
+                <div className={'instructions'}>
+                    <TfiHandPointRight className={'instructionBullets'} />{' '}
+                    <span>
+                        {' '}
+                        For any further queries, reach out to Ayush Mishra [
+                        8927898690 ] or Bitan Banerjee [ 7439377598 ] anytime
+                    </span>
+                </div>
+                {/* please scan any one of the two QR codes shown above. If you choose 1st one, then at the time  */}
+
+                <div className={'merchandiseUnderstoodButtonBox'}>
+                    <button
+                        onClick={() =>
+                            props.setVisibleInstructionsModal(
+                                visibleInstructionsModal =>
+                                    !visibleInstructionsModal,
+                            )
+                        }
+                    >
+                        Ok, I understand
+                    </button>
+                </div>
+            </div>
+        </Modal>
+    )
+}
