e06a0c92f91bd287585b67a3b6cae716f5c058de
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-25 07:33:17 +0530

    add: error response and auth validation

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,14 +1,35 @@
 import {orderService} from '@/services/OrderService'
 import {NextRequest, NextResponse} from 'next/server'
+import {defaultAuth} from '@/firebase-admin/firebase-admin'
 
-
+export const runtime = 'nodejs'
 
 /**
  * @param {NextRequest} request Request object containing item details.
  */
 export async function POST(request) {
-    const formdata = await request.formData()
-    const id = await orderService.registerOrder(formdata)
 
-    return NextResponse.json({"OrderID": id}, {status: 201})
+    const authTokenHeader = request.headers.get('Authorization')
+    const authToken = authTokenHeader.split(' ')[1]
+    try {
+        const decodedToken = await defaultAuth.verifyIdToken(
+            authToken,
+            true,
+        )
+
+        const orderData = await request.json()
+        console.log(orderData)
+
+        if(decodedToken.email !== orderData['Email']) {
+            throw new Error('user faking email...')
+        }
+
+        const id = await orderService.registerOrder(orderData)
+
+        return NextResponse.json({"OrderID": id}, {status: 201})
+    } catch(e) {
+        return NextResponse.json(e, {status: 400})
+    }
+
+
 }
