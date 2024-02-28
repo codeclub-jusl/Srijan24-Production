327f72dd9cb32ace7638e95bb38d92264f8cb104
Arindam Halder <halderarindam10000@gmail.com>
2024-02-29 01:29:03 +0530

    :sparkles: Merchandise: Orders visible in user-end
    Merchandise: Orders visible in user-end

--- src/app/merchandise/page.jsx
+++ src/app/merchandise/page.jsx
@@ -13,7 +13,7 @@ import Form from './components/Form'
 import GridBackground from './components/GridBackground'
 import OrderGuidelines from './components/OrderGuidelines'
 
-import { FaHandPointDown } from "react-icons/fa";
+import { FaHandPointDown } from 'react-icons/fa'
 
 export default function Merchendise() {
     const tshirts = useMemo(function () {
@@ -47,6 +47,21 @@ export default function Merchendise() {
     }, [num])
 
     const user = useSelector(state => state.userReducer.user)
+    const [merchOrders, setMerchOrders] = useState([])
+
+    useEffect(() => {
+        if (user) {
+            fetch(`https://ordertrack-srijan24.onrender.com/orders/${user.email}`).then((res)=>{
+                    return res.json()
+                })
+                .then(res => {
+                    setMerchOrders(res)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }, [user])
 
     const [visibleInstructionsModal, setVisibleInstructionsModal] =
         useState(false)
@@ -58,8 +73,8 @@ export default function Merchendise() {
                 setVisibleInstructionsModal={setVisibleInstructionsModal}
             />
 
-            <div className=' h-screen w-full bg-gray-900'>
-                <div className='flex items-center justify-center bg-gray-900'>
+            <div className=' h-screen w-full bg-gray-900 '>
+                <div className='flex items-center justify-center bg-gray-900 flex-col'>
                     {/* Grid background */}
                     <GridBackground />
 
@@ -126,10 +141,31 @@ export default function Merchendise() {
                         <div className='merchandiseNoteContainer'>
                             <div className='merchandiseNote'>
                                 <h2>Please Note:</h2>
-                                <li>Book your merchandise by <b>7th March</b> to receive it before Srijan'24. </li><li>Orders placed from <b>8th March</b> to <b>14th March</b> will be delivered during the Srijan'24 days that is from 22nd - 24th March.</li>
-                                <li>Orders placed after <b>14th March</b> should be collected from the campus on a later date.</li>
-                                <li>Grab your tshirt under the early bird offer at <b>Rs. 359</b> that is only valid for the first <b>150</b> tshirt orders . Afterward, the price will be <b>Rs. 389</b>. Hurry up!</li>
-                                <li>Register below , the contact details will be given there in <FaHandPointDown /></li>
+                                <li>
+                                    Book your merchandise by <b>7th March</b> to
+                                    receive it before Srijan'24.{' '}
+                                </li>
+                                <li>
+                                    Orders placed from <b>8th March</b> to{' '}
+                                    <b>14th March</b> will be delivered during
+                                    the Srijan'24 days that is from 22nd - 24th
+                                    March.
+                                </li>
+                                <li>
+                                    Orders placed after <b>14th March</b> should
+                                    be collected from the campus on a later
+                                    date.
+                                </li>
+                                <li>
+                                    Grab your tshirt under the early bird offer
+                                    at <b>Rs. 359</b> that is only valid for the
+                                    first <b>150</b> tshirt orders . Afterward,
+                                    the price will be <b>Rs. 389</b>. Hurry up!
+                                </li>
+                                <li>
+                                    Register below , the contact details will be
+                                    given there in <FaHandPointDown />
+                                </li>
                             </div>
                         </div>
 
@@ -142,6 +178,103 @@ export default function Merchendise() {
                                 </Link>
                             </div>
                         )}
+                    </div>
+                    <div className='merchandiseOrderContainer'>
+                        <h2>Merchandise Orders</h2>
+                        <div className='merchBoxes'>
+                            {merchOrders &&
+                                merchOrders.length > 0 &&
+                                merchOrders.map(order => {
+                                    return (
+                                        <div className='merchBox' key={order._id}>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Order ID:
+                                                </div>
+                                                <div className='ans'>
+                                                    {order._id}
+                                                </div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Name:
+                                                </div>
+                                                <div className='ans'>
+                                                    {order.name}
+                                                </div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Phone:
+                                                </div>
+                                                <div className='ans'>
+                                                    {order.phoneNumber}
+                                                </div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    College:
+                                                </div>
+                                                <div className='ans'>
+                                                    {order.college}
+                                                </div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Dept:
+                                                </div>
+                                                <div className='ans'>{order.department}</div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Name on Tshirt:
+                                                </div>
+                                                <div className='ans'>{order.tShirtName}</div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Color of Tshirt:
+                                                </div>
+                                                <div className='ans'>{order.tShirtColor}</div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Tshirt Size:
+                                                </div>
+                                                <div className='ans'>{order.tShirtSize}</div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Payment Collector:
+                                                </div>
+                                                <div className='ans'>
+                                                    {order.paymentCollector}
+                                                </div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Mode of Payment:
+                                                </div>
+                                                <div className='ans'>{order.paymentMode}</div>
+                                            </div>
+                                            <div className='merchRow'>
+                                                <div className='ques'>
+                                                    Transaction ID:
+                                                </div>
+                                                <div className='ans'>
+                                                    {order.transactionID}
+                                                </div>
+                                            </div>
+                                            <div
+                                                className={`merchStatusRow ${order.status}`}
+                                            >
+                                                {order.status}
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                                {merchOrders && merchOrders.length===0 && <div className='noMerchOrder'>No order placed yet</div>}
+                        </div>
                     </div>
                 </div>
             </div>
