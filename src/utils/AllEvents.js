314719dccfc3859bd7507a3063826f3d672c7a27
Arindam Halder <halderarindam10000@gmail.com>
2024-03-20 15:33:21 +0530

    :rocket: OpenAImer regs opened
    OpenAImer regs opened

--- src/utils/AllEvents.js
+++ src/utils/AllEvents.js
@@ -8,7 +8,7 @@ const events = [
         },
         eventType: 'coding',
         eventHashtags: ['hackathon', 'machinelearning', 'ai', 'openaimer'],
-        registrationOpen: false,
+        registrationOpen: true,
         eventCoordinators: [
             'Rajdeep Khaskel [ 6290241875 ]',
             'Sounak Ghosh [ 8910272026 ]',
